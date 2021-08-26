50ce6b225286383a06905a9d76c0e7fd1a925d8c
João Lacerda <joaolacerda1862@gmail.com>
2021-08-26 20:45:19 -0300

    Primeiro controller a páginas Razor testes

--- SalesWebMVC/Controllers/HomeController.cs
+++ SalesWebMVC/Controllers/HomeController.cs
@@ -13,7 +13,8 @@ namespace SalesWebMVC.Controllers {
         }
 
         public IActionResult About() {
-            ViewData["Message"] = "Your application description page.";
+            ViewData["Message"] = "Salles Web MVC App from C# Course";
+            ViewData["Aluno"] = "João";
 
             return View();
         }
